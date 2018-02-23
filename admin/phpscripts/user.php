2c59502d790ce2719c62f4f0d7be2082455eeeab
MACBOOK <c_mai3@fanshaweonline.ca>
2018-02-22 22:31:49 -0500

    minor updates

--- admin/phpscripts/user.php
+++ admin/phpscripts/user.php
@@ -3,8 +3,10 @@
 function createUser($fname, $username, $password, $email, $lvllist){
 include('connect.php');
 
+//Make the system generate the password randomly
 $random = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+;:,.?";
 $password = substr(str_shuffle($random),0,8);
+//then encrypt it ( OMG I can't believe I actually figured the if out to actually make it work on the randomly generated password!)
 if($password){
   $encrypt_pass = password_hash($password, PASSWORD_BCRYPT);
   $password = $encrypt_pass;
@@ -18,9 +20,10 @@ $userquery = mysqli_query($link, $userstring);
 
 if($userquery){
   redirect_to('admin_index.php');
+  //need to figure out the log in URL
   $to = $email;
   $subj = "Your account information - Please log in";
-  $msg = "Your Username: ".$username."\n\nYour Password: ".$password."\n\nLog In Here: ".$url;
+  $msg = "Your Username: ".$username."\n\nYour Password: ".$password."\n\nLog In Here: ".$login_link;
   mail($to,$subj,$msg);
   //echo $msg;
 
